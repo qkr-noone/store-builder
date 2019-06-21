4cccebd6b070f7434dc18af6ff5c6d1bff799af7
Peng <896000867@qq.com>
2019-06-21 16:21:25 +0800

    更新打包生产环境-为Https

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,7 +1,7 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  BASE_API: '"http://seller.mktail.cn:8082"', // 接口：http://192.168.1.100:8082
-  BASE_AFTER: '"http://seller.mktail.cn"', // 后台系统
-  BASE_FRONT: '"http://portal.mktail.cn"' // 用户前台
+  BASE_API: '"https://seller.mktail.cn:8482"', // 接口：http://192.168.1.100:8082
+  BASE_AFTER: '"https://seller.mktail.cn"', // 后台系统
+  BASE_FRONT: '"https://www.mktail.cn"' // 用户前台
 }
