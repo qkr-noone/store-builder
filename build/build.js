5d10a38ece402d34933597258c9e2525f498f67d
Peng <896000867@qq.com>
2019-06-24 13:49:38 +0800

    新增内测环境inner的配置及修改内测配置文件内容

--- build/build.js
+++ build/build.js
@@ -11,7 +11,8 @@ const webpack = require('webpack')
 const config = require('../config')
 const webpackConfig = require('./webpack.prod.conf')
 
-const spinner = ora('building for production...')
+// const spinner = ora('building for production...')
+const spinner = ora('building for ' + process.env.env_config)
 spinner.start()
 
 rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {
