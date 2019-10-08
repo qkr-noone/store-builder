6e7c1e72acdbe78050d7fca06ceee7a268aab3b1
Peng <896000867@qq.com>
2019-10-08 11:18:44 +0800

    修改内测配置文件，采用新的内测地址

--- config/inner.env.js
+++ config/inner.env.js
@@ -1,7 +1,7 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  BASE_API: '"http://192.168.1.100:8082"', // 接口：http://192.168.1.100:8082
-  BASE_AFTER: '"http://192.168.1.100:9002"', // 后台系统
-  BASE_FRONT: '"http://192.168.1.100:9003"' // 用户前台
+  BASE_API: '"https://back.seller.inner.mktail.cn"', // 接口：http://192.168.1.100:8082
+  BASE_AFTER: '"https://seller.inner.mktail.cn"', // 后台系统
+  BASE_FRONT: '"https://www.inner.mktail.cn"' // 用户前台
 }
